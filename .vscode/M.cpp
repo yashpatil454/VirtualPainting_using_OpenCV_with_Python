c0b4eb755efd261f1b51474a85ee99c7a469516e
yashpatil454 <yashpatil454@gmail.com>
2020-12-22 14:03:34 +0530

    first commit

---
+++ .vscode/M.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int i, j, counter = 0; 
+    for (i = 0; i < height; i++) { 
+        printf("*"); 
+        for (j = 0; j <= height; j++) { 
+            if (j == height) 
+                printf("*"); 
+            else if (j == counter 
+                     || j == height - counter - 1) 
+                printf("*"); 
+            else
+                printf(" "); 
+        } 
+        if (counter == height / 2) { 
+            counter = -99999; 
+        } 
+        else
+            counter++; 
+        printf("\n"); 
+    } 
+} 
